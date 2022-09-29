214ef7be86b70a42e83650761246ae45772eaf65
Filype <lyperodrigues03@gmail.com>
2022-09-29 16:43:49 -0300

    Missing

---
+++ Startup.cs
@@ -0,0 +1,26 @@
+
+using MsEFCoreMVC.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace MsEFCoreMVC {
+  public class Startup {
+    public IConfiguration _configuration { get; }
+
+    public Startup(IConfiguration configuration) {
+      _configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services) {
+      services.AddDbContext<SchoolContext>(options =>
+        options.UseNpgsql(_configuration.GetConnectionString("DefaultConnection")));
+
+      services.AddDatabaseDeveloperPageExceptionFilter();
+
+      services.AddControllersWithViews();
+    }
+
+    public void Configure() {
+
+    }
+  }
+}
