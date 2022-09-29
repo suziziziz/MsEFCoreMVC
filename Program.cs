6b167ec4d353a5c40b4926ef8ac985060a2a1c91
Filype <lyperodrigues03@gmail.com>
2022-09-29 16:42:13 -0300

    PT1 Checkpoint

--- Program.cs
+++ Program.cs
@@ -1,27 +1,66 @@
-var builder = WebApplication.CreateBuilder(args);
+using MsEFCoreMVC;
+using MsEFCoreMVC.Data;
+using Microsoft.Extensions.DependencyInjection;
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+namespace MsEFCoreMVC {
+  public class Program {
+    public static void Main(string[] args) {
+      var builder = WebApplication.CreateBuilder(args);
+      var startup = new Startup(builder.Configuration);
+      // var host = CreateHostBuilder(args).Build();
 
-var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+      // host.Run();
+      // System.Console.WriteLine("Test");
+
+      // Add services to the container.
+      builder.Services.AddControllersWithViews();
+
+      startup.ConfigureServices(builder.Services);
+      var app = builder.Build();
+      // startup.Configure(app, builder.Environment);
+      CreateDbIfNotExists(builder.Build());
+
+      // Configure the HTTP request pipeline.
+      if (!app.Environment.IsDevelopment()) {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+      }
+
+      app.UseHttpsRedirection();
+      app.UseStaticFiles();
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.MapControllerRoute(
+          name: "default",
+          pattern: "{controller=Home}/{action=Index}/{id?}");
+
+      app.Run();
+    }
+
+    private static void CreateDbIfNotExists(IHost host) {
+      using (var scope = host.Services.CreateScope()) {
+        var services = scope.ServiceProvider;
+        try {
+          var context = services.GetRequiredService<SchoolContext>();
+          DbInitializer.Initialize(context);
+        }
+        catch (Exception ex) {
+          var logger = services.GetRequiredService<ILogger<Program>>();
+          logger.LogError(ex, "An error occurred creating the DB.");
+        }
+      }
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) {
+      return Host.CreateDefaultBuilder(args)
+          .ConfigureWebHostDefaults(webBuilder => {
+            webBuilder.UseStartup<Startup>();
+          });
+    }
+  }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
