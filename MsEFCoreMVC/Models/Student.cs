ec52f6b95d7a0e001b27b1ef4c8880cd9995ee29
Filype <lyperodrigues03@gmail.com>
2022-09-30 13:35:17 -0300

    CRUD tutorial finished

--- MsEFCoreMVC/Models/Student.cs
+++ MsEFCoreMVC/Models/Student.cs
@@ -1,12 +1,18 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace MsEFCoreMVC.Models {
   public class Student {
     public int ID { get; set; }
     public string LastName { get; set; }
     public string FirstMidName { get; set; }
-    public DateTime EnrollmentDate { get; set; }
+
+    private DateTime _enrollmentDate;
+    public DateTime EnrollmentDate {
+      get => _enrollmentDate;
+      set => _enrollmentDate = value.ToUniversalTime();
+    }
 
     public ICollection<Enrollment> Enrollments { get; set; }
   }
